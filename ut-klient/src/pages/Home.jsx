5094d1aaf2d85664f5109455ad0f6f3e3c5c3dbd
Kneebone <kayern3@hotmail.com>
2026-01-07 23:03:12 +0100

    Testing Heroku Automatic Deploys

--- ut-klient/src/pages/Home.jsx
+++ ut-klient/src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import PageWrapper from "../components/PageWrapper";
 
 export default function Home() {
-  return <PageWrapper title="Velkommen til UT.ut" />;
+  return (
+    <div>
+      <PageWrapper title="Velkommen til UT.ut" />
+      <h1>Dette er en test for å se om Heroku fungerer..</h1> 
+    </div>
+  );
 }
