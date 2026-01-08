c3c480711981decac06f55569b963e33dd18c8ce
Kneebone <kayern3@hotmail.com>
2026-01-08 12:56:32 +0100

    .

--- ut-klient/src/pages/Test.jsx
+++ ut-klient/src/pages/Test.jsx
@@ -29,6 +29,7 @@ export default function Test() {
   }, []);
 
   return (
+    <div>
     <PageWrapper title="Testside">
       <div className="mt-3">
         <h2>Database Test</h2>
@@ -56,5 +57,6 @@ export default function Test() {
         )}
       </div>
     </PageWrapper>
+    </div>
   );
 }
