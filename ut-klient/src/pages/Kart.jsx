6d222f7d23b3cfa5a431025a920a67ca22d93477
Kneebone <kayern3@hotmail.com>
2026-01-30 10:24:18 +0100

    Basic kart

--- ut-klient/src/pages/Kart.jsx
+++ ut-klient/src/pages/Kart.jsx
@@ -1,5 +1,47 @@
-import PageWrapper from "../components/PageWrapper";
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import { Icon } from "leaflet";
+import hytteMarker from "../assets/kart/hytte.png";
+import "../App.css";
+import "leaflet/dist/leaflet.css";
 
-export default function Kart() {
-  return <PageWrapper title="Nettside for kart" />;
+
+
+
+
+export default function kart_Basic(){
+  //simulere hytter
+  const hytter_lokasjon = [
+    {
+      geocode: [59.4087, 9.0593],
+      popUp: "Hytte 1"
+    },
+    {
+      geocode: [59.403166255660764, 9.070148700479844],
+      popUp: "Hytte 2"
+    },
+    {
+      geocode: [59.4111911162318, 9.070644187330702],
+      popUp: "Hytte 3"
+    },
+  ];
+
+  const hytteIcon = new Icon({
+    iconUrl: hytteMarker,
+    iconSize: [36, 36]
+  })
+
+    return(
+        <MapContainer center={[59.4087, 9.0593]} zoom={13}>
+        <TileLayer 
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {hytter_lokasjon.map((hytte) => (
+          <Marker position={hytte.geocode} icon={hytteIcon}>
+            <Popup>{hytte.popUp}</Popup> 
+          </Marker>
+        ))}
+
+        </MapContainer>
+    );
 }
