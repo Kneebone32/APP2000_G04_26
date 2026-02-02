9a11539bbb2496a19cff4c848e9927b768a346fe
Olai Hollekim <olaihhollekim@hotmail.com>
2026-02-02 12:53:10 +0100

    Bugfix

--- ut-klient/src/pages/Admin.jsx
+++ ut-klient/src/pages/Admin.jsx
@@ -42,7 +42,6 @@ export default function Admin() {
             }
 
             alert("Hytte er blitt lagt til.");
-            setHytteId("");
             setNavn("");
             setSengeplasser("");
             refetch(); // Oppdater listen
