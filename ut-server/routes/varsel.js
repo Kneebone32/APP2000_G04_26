f11a75bf645a174e817604513d525672768b14a5
Brainslug <sanderalgry@outlook.com>
2026-03-30 12:30:16 +0200

    Modify varsel.js to fetch alerts for logged-in user
    Updated query to fetch alerts for the logged-in user.

--- ut-server/routes/varsel.js
+++ ut-server/routes/varsel.js
@@ -5,5 +5,5 @@ SELECT public.bruker_rolle_foresporsel_opprett(bruker_id, rolle_id, mottaker_id)
 //Varsel kan godkjenne eller avslå oppgaver 
 SELECT public.varsel_oppgave_behandle(varsel_id, true/false);
 
-//Hent informasjon fra varsel
-SELECT * FROM public.varsel_hent_alle();
+//Hent varselene til innlogget bruker
+SELECT * FROM public.varsel_hent_alle($1);
