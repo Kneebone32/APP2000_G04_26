c150fe0f088e9ab3f63d9fe032439144a1d6c4e4
Kneebone <kayern3@hotmail.com>
2026-04-01 17:52:48 +0200

    påske

--- ut-klient/src/pages/Home.jsx
+++ ut-klient/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import PageWrapper from "../components/PageWrapper";
 import { useTranslation } from "react-i18next";
+import påske from "../assets/påske1.gif";
 import "./Home.css";
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
     <div className="Home">
       <PageWrapper title={t("velkommen")}> 
         <h1>God påske!</h1>
-        <img src="../public/testfiler/påske1.gif" />
+        <img src={påske} />
         </PageWrapper>
     </div>
   );
