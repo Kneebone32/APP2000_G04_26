f5cb197737865964d02aa58e5e3681fba0378bde
Kneebone <kayern3@hotmail.com>
2026-04-01 17:38:34 +0200

    påske?

--- ut-klient/src/pages/Home.jsx
+++ ut-klient/src/pages/Home.jsx
@@ -6,7 +6,10 @@ export default function Home() {
   const { t } = useTranslation();
   return (
     <div className="Home">
-      <PageWrapper title={t("velkommen")} />
+      <PageWrapper title={t("velkommen")}> 
+        <h1>God påske!</h1>
+        <img src="../public/testfiler/påske1.gif" />
+        </PageWrapper>
     </div>
   );
 }
